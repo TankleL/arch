85c2e3970109287d63197ef3f199bd30d5d3ee2c
tain <tankle_@hotmail.com>
2020-02-14 04:39:41 +0800

    refactoring

---
+++ code/archserver/inc/pre-req.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <exception>
+
--- code/archserver/src/archserver.cpp
+++ code/archserver/src/archserver.cpp
@@ -1,35 +1,8 @@
-#include "archserver-prereq.hpp"
-#include "config.hpp"
-#include "dllutils.hpp"
-#include "archmessage.hpp"
-#include "module.hpp"
-#include "tcpserver.hpp"
-
-using namespace std;
-using namespace arch;
+#include "pre-req.hpp"
 
 int main(int argc, char** argv)
 {
-#if defined(_DEBUG) || defined(DEBUG)
-	config::load_config(TEST_PATH_CONFIG_FILE);
-#else
-	config::load_config(argv[1]);
-#endif
-
-	ModuleManager	module_mgr;
-	module_mgr.load_all_modules();
-	
-	ArchMessageQueue	ique, oque;
-	TCPServer	server(&ique, &oque);
-	
-	server.run(
-		&module_mgr,
-		config::server_phase.ipaddr,
-		config::server_phase.port,
-		config::server_phase.backlog,
-		config::server_phase.core_num);
-
-	module_mgr.unload_all_modules();
-
 	return 0;
 }
+
+
